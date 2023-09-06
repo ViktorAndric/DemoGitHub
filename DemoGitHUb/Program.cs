10ae0deb98a58e144eb0dbfa19bba1c941214dbc
Min Dator <Min Dator@DESKTOP-3DRICQ3>
2023-09-06 14:18:12 +0200

    Add project files.

---
+++ DemoGitHUb/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoGitHUb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Viktor!");
+        }
+    }
+}
